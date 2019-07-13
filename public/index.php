789fc805af67410195e77225fc412c7f700496fe
Sander in 't Veld <sanderintveld@hotmail.com>
2019-07-13 19:12:22 +0200

    Working on a nice archetype overview page in HTML+CSS.

---
+++ public/index.php
@@ -0,0 +1,242 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+
+	<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/mana-font@latest/css/mana.css" type="text/css">
+
+	<link rel="stylesheet" href="css/layout.css" type="text/css">
+	<link rel="stylesheet" href="css/fonts.css" type="text/css">
+	<link rel="stylesheet" href="css/text-style.css" type="text/css">
+	<link rel="stylesheet" href="css/color.css" type="text/css">
+	<title>Bucket Cube</title>
+</head>
+<body>
+	<div id="center_wrapper">
+
+	<div id="main_wrapper">
+
+		<div class="image_wrapper">
+			<div class="right">
+				<img src="https://img.scryfall.com/cards/art_crop/front/1/1/1173ff96-998c-4fe7-9b28-602d990e0339.jpg?1562814843" alt="">
+			</div>
+		</div>
+
+		<h1 id="title">Bucket Cube</h1>
+
+		<p>
+			I bought a bucket of cards and tried to make a peasant cube out of it. No rares, no planeswalkers and no counterspells.
+		</p>
+
+		<p>
+			Analyzed using my <a href="https://github.com/SLiV9/mtg-cube-analysis">cube complexity analyzer</a>.
+			There is a visual spoiler on <a href="https://deckstats.net/decks/20660/120088-bucket-of-commons-cube">deckstats.net</a>.
+		</p>
+
+		<p>
+			Slight shifted color identities: <i class="ms ms-w"></i> favors aggression, <i class="ms ms-r"></i> favors big creatures, <i class="ms ms-g"></i> favors graveyard, <i class="ms ms-b"></i> favors disruption and <i class="ms ms-u"></i> favors enchantments.
+		</p>
+
+	<table>
+
+<tr>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/a/3/a353510a-30de-4891-97b9-d7d556531c41.jpg?1562303439" alt="">
+		<h2><i class="ms ms-r"></i><i class="ms ms-g"></i>
+			Ramp
+		</h2>
+		<p>
+			Play beefy creatures and use burn spells to survive until the late game, when your ramping pays off with massive creatures and a lot of mana sinks.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/d/a/da3da672-cf12-4eb9-be68-aaa0e3cbf5d8.jpg?1562276070" alt="">
+		<h2><i class="ms ms-b"></i><i class="ms ms-r"></i>
+			Tempo Control
+		</h2>
+		<p>
+			With both red burn and black removal at your disposal, you can keep your opponent's creatures off the board while you attack with your own.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/e/4/e43d959f-6055-4578-a69a-0ec93e993e21.jpg?1562937669" alt="">
+		<h2><i class="ms ms-g"></i><i class="ms ms-w"></i>
+			Wide Midrange
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/6/7/675640ba-37e7-4231-8524-87e8b87ea46f.jpg?1538879098" alt="">
+		<h2><i class="ms ms-u"></i><i class="ms ms-b"></i>
+			Control
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/6/2/628213e9-bde9-43fd-a0d9-8c7fb17be879.jpg?1562644868" alt="">
+		<h2><i class="ms ms-w"></i><i class="ms ms-u"></i>
+			Flyers
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+</tr>
+<tr>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/6/b/6b40108a-c9b6-4364-9d5a-c87a9cc4e94b.jpg?1562274137" alt="">
+		<h2><i class="ms ms-r"></i><i class="ms ms-g"></i>
+			Power matters
+		</h2>
+		<p>
+			All of your creatures have innately more power than your opponent's creatures, or they have firebreathing. Your spells become stronger based on the highest power among creatures you control.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/9/1/9160dde8-cd77-4967-a5f3-a676376c58f7.jpg?1562302975" alt="">
+		<h2><i class="ms ms-b"></i><i class="ms ms-r"></i>
+			Sacrifice
+		</h2>
+		<p>
+			Trade small creatures for extremely powerful spells, or use your opponent's creatures for that purpose. Once your opponent is low, sac your board to kill them.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/0/2/0271a2bc-19c8-423b-8702-d0bfe52b3580.jpg?1562272014" alt="">
+		<h2><i class="ms ms-g"></i><i class="ms ms-w"></i>
+			+1/+1 counters
+		</h2>
+		<p>
+			Keep your creatures alive for Convoke and then buff them using Outlast. Certain key creatures give bonusses to all of your creatures with +1/+1 counters on them.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/c/4/c4628454-65f0-4cf7-82b5-3c33e0b3a705.jpg?1561768426" alt="">
+		<h2><i class="ms ms-u"></i><i class="ms ms-b"></i>
+			Mill
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/4/c/4c929a57-2c23-4bbf-b265-666630a4fde8.jpg?1562817817" alt="">
+		<h2><i class="ms ms-w"></i><i class="ms ms-u"></i>
+			Heroic matters
+		</h2>
+		<p>
+			Use enchantments to build stronger creatures. Heroic creatures grant much needed card advantage and Bestow creatures ensure a good mix of enchantments and enchantees.
+		</p>
+	</td>
+</tr>
+<tr>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/1/0/1013ca9c-1d29-42f6-8665-92f98d076ff8.jpg?1562898365" alt="">
+		<h2><i class="ms ms-r"></i><i class="ms ms-w"></i>
+			Aggro
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/1/0/100855e6-ac3e-481f-a462-afb5fcb5d476.jpg?1562432933" alt="">
+		<h2><i class="ms ms-b"></i><i class="ms ms-g"></i>
+			Control Ramp
+		</h2>
+		<p>
+			You play big defensive creatures so your opponent cannot attack you. You are free to trade because you can bring your best creatures back in the late game.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/a/a/aae50a9a-1589-43fe-87a6-7b2c96745392.jpg?1562439668" alt="">
+		<h2><i class="ms ms-g"></i><i class="ms ms-u"></i>
+			Value Ramp (?)
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/c/2/c284997a-cfc3-4ffc-bc25-00c130fb1eb0.jpg?1562748649" alt="">
+		<h2><i class="ms ms-w"></i><i class="ms ms-b"></i>
+			??? (Lifegain Tempo)
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/0/a/0ae987da-e4b5-4b82-843c-6aaa87262fc8.jpg?1538880243" alt="">
+		<h2><i class="ms ms-u"></i><i class="ms ms-r"></i>
+			Value Tempo (?)
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+</tr>
+<tr>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/7/d/7d47bcb1-b028-4be8-9710-7c390cb984ce.jpg?1562920282" alt="">
+		<h2><i class="ms ms-r"></i><i class="ms ms-w"></i>
+			Equipment matters
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/6/0/605038e9-2840-4c2d-bd7a-1004ae210e2a.jpg?1547517517" alt="">
+		<h2><i class="ms ms-b"></i><i class="ms ms-g"></i>
+			Graveyard matters
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/1/e/1efbf7be-bc11-4285-a37d-83790b292b3a.jpg?1562839818" alt="">
+		<h2><i class="ms ms-g"></i><i class="ms ms-u"></i>
+			Evasive fatty (?)
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/c/d/cd1131c6-04da-4c4d-ab61-874ac5be7087.jpg?1562933372" alt="">
+		<h2><i class="ms ms-w"></i><i class="ms ms-b"></i>
+			Weenies
+		</h2>
+		<p>
+			This is nice.
+		</p>
+	</td>
+	<td>
+		<img src="https://img.scryfall.com/cards/art_crop/front/6/e/6e0def77-3528-40fb-a6b2-c3d1e31ade65.jpg?1562302676" alt="">
+		<h2><i class="ms ms-u"></i><i class="ms ms-r"></i>
+			Prowess matters
+		</h2>
+		<p>
+			Keep casting instants and sorceries to buff your Prowess creatures and keep your card draw engine alive.
+		</p>
+</tr>
+
+	</table>
+
+	</div>
+
+	<div class="footer">
+		<div class="text">
+			For internal use only.</a>
+		</div>
+	</div>
+
+	</div>
+</body>
+</html>
